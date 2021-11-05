b2157c0a56f0cf44e06e969e4985d842e3740721
justin <17525874+justinnk@users.noreply.github.com>
2021-11-05 10:50:45 +0100

    Remove old leftover files.

--- extension/run_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Starts an example simulation utilizing the aspect-oriented next reaction method.
-
-classpath="target/extension-0.0.2-SNAPSHOT.jar:target/dependency/*"
-
-mvn clean package dependency:copy-dependencies
-java -cp "$classpath" org.justinnk.ssamason.demo.ssamason.sir.SirModelWithUI
