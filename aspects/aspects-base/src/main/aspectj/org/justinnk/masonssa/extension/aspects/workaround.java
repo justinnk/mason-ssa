15cb393a8c7ec89205ede2c9d770226dfa203d64
justinnk <17525874+justinnk@users.noreply.github.com>
2021-12-14 14:19:38 +0100

    Add workaround to generate javadoc for aspect modules. Fix deployment.

---
+++ aspects/aspects-base/src/main/aspectj/org/justinnk/masonssa/extension/aspects/workaround.java
@@ -0,0 +1,6 @@
+package org.justinnk.masonssa.extension.aspects;
+
+/** This is a workaround to generate javadoc for this module until the generation of javadoc for aspectj is fixed. */
+public class workaround {
+
+}
